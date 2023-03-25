d89c31d453ca5ac9f677dc0e9dfa545ffd0150fc
Taha <tahaavarol@hotmail.com>
2023-03-25 20:16:34 +0300

    Add files via upload

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -3,24 +3,37 @@ import { getMovies } from "../services/api";
 import { handleSearch, handleSort } from "../utils/helper";
 
 export default function Serias() {
+  //movies: Filmlerin listesini tutan state değişkenidir. İlk başta boş bir dizi ile başlatılır ve daha sonra getMovies() fonksiyonu ile bu değişkene değer atanır.
   const [movies, setMovies] = useState([]);
+  //searchTerm: Arama çubuğundaki kullanıcının girdiği metni tutan state değişkenidir. 
+  //İlk başta boş bir string ile başlatılır ve daha sonra handleSearchInputChange() fonksiyonu ile bu değişkene yeni değer atanır.
   const [searchTerm, setSearchTerm] = useState("");
-  const [sortType, setSortType] = useState("asc");
+  //sortType: Sıralama seçeneğini tutan state değişkenidir. 
+  //İlk başta "asc" (artan) olarak başlatılır ve daha sonra handleSortButtonClick() fonksiyonu ile bu değişkene yeni değer atanır.
+  const [sortType, setSortType] = useState("");
 
+  //"useEffect" hooku ile component ilk kez render edildiğinde "getMovies()" fonksiyonu ile datayı alır. 
+  //"[]" argümanı ile yapılmak istenen,"useEffect fonksiyonunu bir kez çalışmasını sağlamak bu sayede "getMovies()",
+  // fonksiyonu sadece sayfa ilk yüklendiğinde çalışır sayfa güncellendiğinde herseferinde "getMovies()" çağrılmaz
   useEffect(() => {
     getMovies().then((data) => setMovies(data));
   }, []);
 
+  //Inputa değer girdiğinde tetiklenen bir fonksiyondur. Değer tutar ve "setSearchTerm" fonksiyonu içine atar
   const handleSearchInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
-
+   //Dropdown de seçilen butonun value değerini tutar. Değeri "setSortType" fonksiyonu içine atar
   const handleSortButtonClick = (event) => {
     setSortType(event.target.value);
   };
 
+
+  //"filteredMovies" değişkeni, "handleSearch" yardımcı fonksiyonunu kullanarak "movies" dizisindeki filmler arasından kullanıcının arama terimine göre filtrelenmiş olan filmleri içerir.
   const filteredMovies = handleSearch(searchTerm, movies);
+  //"sortedMovies" değişkeni, "handleSort" yardımcı fonksiyonunu kullanarak "filteredMovies" dizisini "sortType" değişkeninin değerine göre sıralar
   const sortedMovies = handleSort(sortType, filteredMovies);
+  //"moviesToDisplay" değişkeni, sıralanmış "sortedMovies" dizisinin ilk 18 öğesini içerir. Böylece sayfada sadece ilk 18 film gösterilir.
   const moviesToDisplay = sortedMovies.slice(0, 18);
 
   return (
